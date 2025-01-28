60ceff79c774c9793546621e18543eb55d2b41a5
Kartik Lolla <TheRealChanger@proton.me>
2025-01-29 00:04:40 +0530

    My try...

--- hextobytetob64.py
+++ hextobytetob64.py
@@ -1,6 +1,6 @@
 import base64
 
-hex_string = "48656C6C6F20776F726C64"  # Example hex: "hello world"
+hex_string = "I will bring the change"  # Example hex: "hello world"
 byte_data = bytes.fromhex(hex_string)
 answer = base64.b64encode(byte_data)
 print(answer)
